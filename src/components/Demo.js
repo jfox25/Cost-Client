e4cde87e7bfb807a41519db42b60895c05b0e6ce
James Fox <jimmyfox00@gmail.com>
2022-07-08 11:29:49 -0700

    Now have error handling an no longer need a button to fetch

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,16 +1,27 @@
-import {useState} from "react"
+import {useState, useEffect, useCallback} from "react"
+import TableControl from "./Table/TableControl"
 
 const Demo = () => {
     const [expenses, setExpenses] = useState([]);
-    const fetchExpenses = () => {
-        fetch('https://localhost:5001/api/expenses', {
-            headers: new Headers({
-                "Authorization" : ''
+    const [isloading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const columns = [ "Location", "Category" , "Directive" , "Date", "Cost"]
+    const fetchExpensesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://localhost:5001/api/expenses', {
+                headers: new Headers({
+                    "Authorization" : ''
+                })
             })
-        }).then(response => {
-             return response.json();
-        }).then(data => {
-            console.log(data);
+            if(!response.ok)
+            {
+                throw new Error('Something went wrong')
+            }
+            const data = await response.json()
+
             const transformedExpenses = data.map(expense => { return {
                 id : expense.expenseId,
                 Location : expense.locationName,
@@ -20,14 +31,28 @@ const Demo = () => {
                 Cost : expense.cost,
                 } 
             })
-            setExpenses(transformedExpenses);
-            console.log(expenses);
-        })
+            setExpenses(transformedExpenses)
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoading(false)
+
+    }, [])
+    useEffect(() => {
+        fetchExpensesHandler();
+    }, [fetchExpensesHandler]);
+    let content = <TableControl columns={columns} items={expenses} />;
+    if(error) {
+        content = <p>{error}</p>
+    }
+    if(isloading) {
+        content = <p>Loading ...</p>
     }
     return (
         <div>
-            <button onClick={fetchExpenses}>Fetch Expenses</button>
+            <button onClick={fetchExpensesHandler}>Fetch Expenses</button>
             <h1>Expenses</h1>
+            {content}
         </div>
     )
 }
